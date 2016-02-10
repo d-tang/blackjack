a71b86b3b2f647cd488d791849a8911430128a68
gSchool Student <datascientist@Datas-Mac-mini-2.local>
2016-02-09 17:38:31 -0800

    2nd commit

--- blackjack.py
+++ blackjack.py
@@ -15,10 +15,33 @@ class blackjack(object):
         for i in range(2):
             self.player1.receive_cards(deck.draw_card())
             self.Dealer.receive_cards(deck.draw_card())
+        self.print_value()
+        self.checkIfBuster()
 
     def check_Dealer_hand(self):
         if self.Dealer.hand_values < 17:
             self.Dealer.receive_cards(deck.draw_card())
+
+    def Ask_for_hit(self):
+        ans = raw_input("Another Card (y,n)")
+        if ans == 'y':
+            self.player1.receive_cards(deck.draw_card())
+            self.check_Dealer_hand()
+            self.print_value()
+        else:
+            old_value = 0
+            while old_value != self.Dealer.totalhand:
+                old_value = self.Dealer.totalhand
+                self.check_Dealer_hand()
+            self.determine_winner()
+
+    def determine_winner():
+        if self.player1.totalhand == self.Dealer.totalhand:
+            print ("Tie")
+        elif self.player1.totalhand > self.Dealer.totalhand:
+            print ("Player 1 wins")
+        elif self.player1.totalhand < self.Dealer.totalhand:
+            print ("Dealer wins")
 
     def checkIfBuster(self):
         if self.Dealer.hand_values > 21 and self.player1.hand_values>21:
@@ -28,5 +51,10 @@ class blackjack(object):
         elif self.player1.hand_values > 21:
             return ("Dealer wins")
 
+    def print_value(self):
+        print "Your hand value is: %d" %, self.player1.totalhand
+        return
+
     def playgame(self):
+        
         pass
